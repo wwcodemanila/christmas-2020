71721455c6d9301286e13eb73c4b8f1304fcb04c
kimberrypi <ksmorano@gmail.com>
2020-12-24 07:22:08 +0800

    update card font size

--- src/components/ChristmasCards/Card.js
+++ src/components/ChristmasCards/Card.js
@@ -6,8 +6,12 @@ const Card = ({ name, message }) => {
       className={`border border-gray-200 bg-gray-50 rounded max-w-xs max-h-md p-2 m-3`}
     >
       <div>
-        <div className="font-medium text-gray-900 text-2xl">"{message}"</div>
-        <div className="text-md text-gray-500">- {name}</div>
+        <div className="font-medium text-gray-900 sm:text-md md:text-lg lg:text-lg xl:text-lg 2xl:text-lg">
+          "{message}"
+        </div>
+        <div className="sm:text-sm md:text-md lg:text-md xl:text-lg 2xl:text-lg text-gray-500">
+          - {name}
+        </div>
       </div>
     </div>
   )
